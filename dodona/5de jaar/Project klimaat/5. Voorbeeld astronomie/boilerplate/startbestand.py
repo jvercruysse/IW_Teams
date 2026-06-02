1701f0305b4aa34e0e93c65e139adb0e63c62f9c
jvercruysse <111382823+jvercruysse@users.noreply.github.com>
2026-06-02 14:33:35 +0200

    migrated to 'Sint-Rembert' repo

--- dodona/5de jaar/Project klimaat/5. Voorbeeld astronomie/boilerplate/startbestand.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-stereig = pd.read_csv('stereigenschappen.csv', sep=';')
-
-# VRAAG DE KOLOMMEN MET DE MASSAS EN DE TEMPERATUREN OP
-mass = # ...
-temp = # ...
-
-print(mass)
-print(temp)
